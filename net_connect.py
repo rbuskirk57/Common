938719e0d3202a4d6b4fa66080de6f34478d72f4
rbuskirk57 <125936958+rbuskirk57@users.noreply.github.com>
2026-02-22 15:34:35 -0800

    Update net_connect.py
    Added some info to print for troubleshooting

--- net_connect.py
+++ net_connect.py
@@ -1,11 +1,11 @@
 import network
 from time import sleep
 
-def connect(ssid, password, max_count):
+def connect(ssid, password):
     #Connect to WLAN
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    #max_count = 20
+    max_count = 20
     ip = '-1'
     wlan.connect(ssid, password)
     
@@ -14,11 +14,15 @@ def connect(ssid, password, max_count):
         sleep(1)
         if wlan.isconnected():
             ip = wlan.ifconfig()[0]
-            print(f'Connected on {ip}')
+            print(f'wlan Connected on {ip}')
             break
         else:
             max_count -= 1
 
     if not wlan.isconnected():
         print("Network connection failed")
+        
+    print('RSSI:', wlan.status('rssi'))  # After connection; higher negative = weaker (e.g., -50 good, -80 marginal)
+    print("wlan.iconfig", wlan.ifconfig())
+    
     return ip
